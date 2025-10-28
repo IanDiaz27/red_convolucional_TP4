ed7cd50f8f2f91b1172265dbef2cab20378ef0ea
Ian Diaz <86169056+IanDiaz27@users.noreply.github.com>
2025-10-28 20:50:28 -0300

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,14 @@
+from keras import models, metrics, layers
+
 act="relu"
 
-model = Sequential()
-model.add(Conv2D(input_shape=(200,200,3), kernel_size(3,3), padding="same", strides=(1,1) filters=32))
-model.add(MaxPooling2D(pool_size=(2,2), strides(1,1), padding="same"))
-model.add(Conv2D(kernel_size(3,3), padding="same", strides=(1,1) filters=64))
-model.add(MaxPooling2D(pool_size=(2,2), strides(1,1), padding="same"))
-model.add(Flatten())
-model.add(Dense(256, activation=act))
-model.add(Dense(128, activation=act))
-model.add(Dense(8, activation="softmax"))
+model = models.Sequential()
+model.add(layers.Input(shape=(200, 200, 3)))
+model.add(layers.Conv2D(kernel_size=(3,3), padding="same", strides=(1,1), filters=32))
+model.add(layers.MaxPooling2D(pool_size=(2,2), strides=(1,1), padding="same"))
+model.add(layers.Conv2D(kernel_size=(3,3), padding="same", strides=(1,1), filters=64))
+model.add(layers.MaxPooling2D(pool_size=(2,2), strides=(1,1), padding="same"))
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation=act))
+model.add(layers.Dense(128, activation=act))
+model.add(layers.Dense(8, activation="softmax"))
