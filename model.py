92fff8bb97525f0ac89d677b9ca69f2ae499517e
Manuel Nuñez <112630074+mnunez-unahur@users.noreply.github.com>
2025-11-09 19:13:48 +0000

    arquitectura v1

--- model.py
+++ model.py
@@ -3,74 +3,59 @@ import tensorflow as tf
 import numpy as np
 
 
-def crearRed(ancho_image, alto_imagen, cant_clases, seed=11):
+def crearRed(cant_salidas, input_shape=(200,200,3), seed=11):
     utils.set_random_seed(seed)
 
-    model = keras.Sequential(
+    model = models.Sequential(
         [
-            layers.Input(shape=(ancho_image, alto_imagen, 3)),
-            # data_augmentation,
-            layers.Conv2D(32, 3, activation="relu"),
-            layers.MaxPooling2D(),
-            layers.Conv2D(64, 3, activation="relu"),
-            layers.MaxPooling2D(),
-            layers.Conv2D(128, 3, activation="relu"),
-            layers.MaxPooling2D(),
+            # Capa de entrada
+            layers.Input(shape=input_shape),
+            
+            # Capas de convolución
+            layers.Conv2D(32, kernel_size=(3,3), strides=(1,1), padding="same"),
+            layers.MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same"),
+
+            # layers.Conv2D(64, kernel_size=(3,3), strides=(1,1), padding="same"),
+            # layers.MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same"),
+
             layers.Flatten(),
-            layers.Dense(128, activation="relu"),
-            layers.Dropout(0.5),
-            layers.Dense(cant_clases, activation="softmax"),
+
+            layers.Dense(200, activation="relu"),
+            # layers.Dense(128, activation="relu"),
+
+            # 
+            # layers.Dropout(0.5),
+
+            # Capa de Salida
+            layers.Dense(cant_salidas, activation="softmax"),
         ]
     )
 
+
+
     return model
 
-def cargarImagenesEntrenamiento(path="img", seed=11, batch_size=32):
-    ds = preprocessing.image_dataset_from_directory(
+def cargarImagenesEntrenamiento(path="img", alto=200, ancho=200, seed=11, batch_size=32):
+    ds_entrenamiento, ds_valicacion = preprocessing.image_dataset_from_directory(
         path,
         labels="inferred",
         label_mode="int",
         color_mode="rgb",
-        image_size=(200,200),
+        image_size=(alto,ancho),
         shuffle=True,
         seed=seed,
         validation_split=0.20,
-        subset="training",
+        subset="both",
         batch_size=batch_size
     )
 
-    clases = ds.class_names
+    clases = ds_entrenamiento.class_names
     cantClases = len(clases)
 
     # crea un map agregando una matriz con las respuestas esperadas
-    ds = ds.map(lambda imgs, labels: (imgs, tf.one_hot(labels, depth=cantClases)) )
+    # ds_entrenamiento = ds_entrenamiento.map(lambda imgs, labels: (imgs, tf.one_hot(labels, depth=cantClases)) )
+    # ds_valicacion = ds_valicacion.map(lambda imgs, labels: (imgs, tf.one_hot(labels, depth=cantClases)) )
 
-    return ds, clases
-
-def cargarImagenesValidacion(path="img", seed=11, batch_size=32):
-    ds = preprocessing.image_dataset_from_directory(
-        path,
-        labels="inferred",
-        label_mode="int",
-        color_mode="rgb",
-        image_size=(200,200),
-        shuffle=True,
-        seed=seed,
-        validation_split=0.20,
-        subset="validation",  
-        batch_size=batch_size
-    )
-
-    clases = ds.class_names
-    cantClases = len(clases)
-
-    # crea un map agregando una matriz con las respuestas esperadas
-    ds = ds.map(lambda imgs, labels: (imgs, tf.one_hot(labels, depth=cantClases)) )
-
-    return ds, clases
-
-
-def obtenerMatrizClasificacion(cantClases):
-    return np.eye(cantClases, dtype=float)
+    return ds_entrenamiento, ds_valicacion, clases
 
 
--- punto-1-entrenamiento.py
+++ punto-1-entrenamiento.py
@@ -4,100 +4,67 @@ import time
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-from model import crearRed, cargarImagenesEntrenamiento, cargarImagenesValidacion
+from model import crearRed, cargarImagenesEntrenamiento
 
 
-
-epocas = 500
-batch_size = 8
+num_prueba = input("Número de Prueba: ")
+epocas = 100
+batch_size = 32
 n = 0.001
 seed = 11
-ds_entrenamiento, clases = cargarImagenesEntrenamiento("img", seed=seed, batch_size=batch_size)
-ds_validacion, _ = cargarImagenesValidacion("img", seed=seed, batch_size=batch_size)
+loss="sparse_categorical_crossentropy"
+# loss="categorical_crossentropy"
+# loss="binary_crossentropy"
+
+ds_entrenamiento, ds_validacion, clases = cargarImagenesEntrenamiento(
+    path="datos/entrenamiento", 
+    seed=seed, 
+    batch_size=batch_size)
 
 cantClases = len(clases)
 print("Clases:", clases)
+
 
 # inicio de entrenamiento
 
 # tf.config.set_visible_devices([], device_type='GPU')
 # print("devices:", tf.config.get_visible_devices())
-# tf.config.experimental.enable_op_determinism()
+tf.config.experimental.enable_op_determinism()
 
 
 
-
-model = crearRed(cantClases, seed)
+model = crearRed(cant_salidas=cantClases, seed=seed)
 opt = optimizers.Adam(learning_rate = n)
-# metrica = metrics.BinaryAccuracy(name="binary accuracy", dtype=None, threshold=0.5)
-model.compile(loss = 'categorical_crossentropy', optimizer = opt, metrics = ['accuracy'])
-
-def lr_schedule(epoch, lr):
-    if epoch != 0 and epoch % 10 == 0:
-        return lr * 0.5
-    return lr
-
-lr_scheduler = callbacks.LearningRateScheduler(lr_schedule, verbose=1)
-
-# Opcional: EarlyStopping para evitar sobre‑entrenamiento
-early_stop = callbacks.EarlyStopping(
-    monitor="val_loss",
-    patience=20,
-    restore_best_weights=True
-)
-
-
+model.compile(loss = loss, optimizer = opt, metrics = ['accuracy'])
 
 inicio = time.perf_counter()
 historia = model.fit(ds_entrenamiento, 
                 epochs = epocas,
-                callbacks=[lr_scheduler, early_stop],
                 validation_data=ds_validacion
             )
 fin = time.perf_counter()
-model.save(f"punto-1.keras")
+# model.save(f"punto-1.keras")
 
 print(f"tiempo consumido: {fin - inicio}")
 
-plt.plot(historia.history['loss'])
-plt.title(f"Entrenamiento - Epocas: {epocas} - Batch Size: {batch_size}")
+plt.title(f"Loss - Epocas: {epocas} - Batch Size: {batch_size} - n: {n}")
 plt.xlabel("Épocas")
 plt.ylabel("Pérdida")
-plt.ylim(0, 2)
-plt.legend(["entrenamiento"],loc="upper right")
-plt.savefig(f"punto-1-entrenamiento_loss_{epocas}_{batch_size}_{n}.png")
+plt.plot(historia.history['loss'])
+plt.plot(historia.history['val_loss'])
+plt.legend(['loss', 'val_loss'],loc="upper right")
+plt.savefig(f"resultados/prueba-{num_prueba}_{epocas}_{batch_size}_{n}_{loss}_loss.png")
 
 
-
-resultado = model.predict(ds_entrenamiento)
-
-def equal(a, b):
-    if len(a) != len(b):
-        return False
-    
-    for i in range(len(a)):
-        if a[i] != b[i]:
-            return False
-
-    return True
-
-i = 0
-cantDiferentes = 0
-for (imgs, labels) in ds_entrenamiento:
-    for label in labels:
-        labeObtenido = np.round(resultado[i])
-        labelEsperado = np.round(label.numpy())
-        print(i, labelEsperado , labeObtenido)
-        if not equal( labelEsperado, labeObtenido ):
-            cantDiferentes += 1
-        i += 1
+plt.clf()
+plt.title(f"Accuracy - Epocas: {epocas} - Batch Size: {batch_size} - n: {n}")
+plt.xlabel("Épocas")
+plt.ylabel("Accuracy")
+plt.plot(historia.history['accuracy'])
+plt.plot(historia.history['val_accuracy'])
+plt.legend(['accuracy', 'val_accuracy'],loc="upper right")
+plt.savefig(f"resultados/prueba-{num_prueba}_{epocas}_{batch_size}_{n}_{loss}_accuracy.png")
 
 
-print(i, cantDiferentes, (cantDiferentes/i)*100)
-
-# ds_entrenamiento, clases = cargarImagenesValidacion("img", seed=seed)
-
-
-# model.predict(ds_entrenamiento)
 
 
